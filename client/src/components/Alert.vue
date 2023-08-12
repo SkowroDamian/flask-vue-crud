34be9e34230c266a36a7c9f9599c3d84e3a0e5d2
Damian Skowroński <skowronskid@icloud.com>
2023-08-12 11:58:11 +0200

    finished

---
+++ client/src/components/Alert.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="alert alert-success" role="alert">{{ message }}</div>
+    <br/>
+</template>
+
+<script>
+export default {
+    props: ['message'],
+};
+</script>
---
+++ client/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Ping from '../components/Ping.vue'
+import Books from '../components/Books.vue'
+// import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/ping',
+      name: 'ping',
+      component: Ping,
+    },
+    {
+      path: '/',
+      name: 'Books',
+      component: Books
+    }
+
+  ]
+})
+
+export default router
